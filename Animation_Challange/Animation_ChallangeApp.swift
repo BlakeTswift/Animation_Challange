024ee88a0168e6947ea1360e35dbd0721892aeae
BlakeT <BlakeTswift@gmail.com>
2025-09-25 13:17:34 -0600

    Initial Commit

---
+++ Animation_Challange/Animation_ChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_ChallangeApp.swift
+//  Animation_Challange
+//
+//  Created by Trytten, Blake - Student on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Animation_ChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation_ChallangeTests/Animation_ChallangeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_ChallangeTests.swift
+//  Animation_ChallangeTests
+//
+//  Created by Trytten, Blake - Student on 9/25/25.
+//
+
+import Testing
+@testable import Animation_Challange
+
+struct Animation_ChallangeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
